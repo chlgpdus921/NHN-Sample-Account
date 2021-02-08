7c52c06dee8bd9deb3b0d537783adde860ec9752
hyeyeon-choi <chlgpdus921@naver.com>
2021-02-08 16:14:15 +0900

    commit

---
+++ sample-account/src/main/java/com/nhnent/rookie8/sampleaccount/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.nhnent.rookie8.sampleaccount;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class HelloWorldController {
+
+    @Value("${application.message:Hello World}")
+    private String message = "Hello rookie";
+
+    @GetMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("time", new Date());
+        model.addAttribute("message", this.message);
+
+        return "welcome";
+    }
+
+    @GetMapping("/helloworld")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello Worlddddd";
+    }
+
+}
---
+++ sample-account/src/main/java/com/nhnent/rookie8/sampleaccount/SampleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.nhnent.rookie8.sampleaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAccountApplication.class, args);
+	}
+
+}
